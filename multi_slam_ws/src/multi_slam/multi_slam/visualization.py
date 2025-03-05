ed8ea7f7e37bf34107b442b02932f46636721491
Gavin Yuliu Hua <gavin.hyl@outlook.com>
2025-03-05 15:11:51 +0800

    Add pos to vis.

--- multi_slam_ws/src/multi_slam/multi_slam/visualization.py
+++ multi_slam_ws/src/multi_slam/multi_slam/visualization.py
@@ -5,7 +5,11 @@ from std_msgs.msg import String, Float32MultiArray
 class Visualizer(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.control_input_sub = self.create_subscription(Float32MultiArray, '/control_input', self.control_intput_cb, 10) 
+        self.control_input_sub = self.create_subscription(Float32MultiArray, '/control_input', self.control_intput_cb, 10)
+        self.pos_sub = self.create_subscription(Float32MultiArray, '/true_position', self.pos_cb, 10)
 
     def control_intput_cb(self, msg: Float32MultiArray):
         self.get_logger().info(f"Received control input: {msg.data}")
+
+    def pos_cb(self, msg: Float32MultiArray):
+        self.get_logger().info(f"Received position: {msg.data}")
