3ea90a3b4447b28615ef0b432b0269d5bd52582f
Gavin Yuliu Hua <gavin.hyl@outlook.com>
2025-03-05 14:47:53 +0800

    Add requirements.txt

---
+++ multi_slam_ws/src/multi_slam/multi_slam/visualization.py
@@ -0,0 +1,17 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.listener_callback,
+            10)
+        # Prevent unused variable warning.
+        self.subscription
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'I heard: {msg.data}')
