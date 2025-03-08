64b3dc77488a29f95b6d49067d184682e7bd1333
Gavin Yuliu Hua <gavin.hyl@outlook.com>
2025-03-08 10:36:35 +0800

    Fix format with ruff.

--- multi_slam_ws/src/multi_slam/multi_slam/PhysicsSimNode.py
+++ multi_slam_ws/src/multi_slam/multi_slam/PhysicsSimNode.py
@@ -11,33 +11,35 @@ from visualization_msgs.msg import Marker
 
 class PhysicsSimNode(Node):
     def __init__(self):
-        super().__init__('physics_sim')
-        self.control_signal_sub = self.create_subscription(Vector3, 'control_signal', self.control_signal_cb, 10)
+        super().__init__("physics_sim")
+        self.control_signal_sub = self.create_subscription(
+            Vector3, "control_signal", self.control_signal_cb, 10
+        )
 
-        self.declare_parameter('lidar_r_max', 5)
-        self.lidar_r_max = self.get_parameter('lidar_r_max').value
+        self.declare_parameter("lidar_r_max", 5)
+        self.lidar_r_max = self.get_parameter("lidar_r_max").value
 
-        self.declare_parameter('lidar_r_min', 0.2)
-        self.lidar_r_min = self.get_parameter('lidar_r_min').value
+        self.declare_parameter("lidar_r_min", 0.2)
+        self.lidar_r_min = self.get_parameter("lidar_r_min").value
 
-        self.declare_parameter('lidar_delta_theta', 3)
-        self.lidar_delta_theta = self.get_parameter('lidar_delta_theta').value
+        self.declare_parameter("lidar_delta_theta", 3)
+        self.lidar_delta_theta = self.get_parameter("lidar_delta_theta").value
 
-        self.declare_parameter('lidar_std_dev', 0.1)
-        self.lidar_std_dev = self.get_parameter('lidar_std_dev').value
+        self.declare_parameter("lidar_std_dev", 0.1)
+        self.lidar_std_dev = self.get_parameter("lidar_std_dev").value
 
-        self.declare_parameter('beacon_std_dev', 0.1)
-        self.beacon_std_dev = self.get_parameter('beacon_std_dev').value
+        self.declare_parameter("beacon_std_dev", 0.1)
+        self.beacon_std_dev = self.get_parameter("beacon_std_dev").value
 
-        self.declare_parameter('sim_dt', 0.1)
-        self.sim_dt = self.get_parameter('sim_dt').value
+        self.declare_parameter("sim_dt", 0.1)
+        self.sim_dt = self.get_parameter("sim_dt").value
 
-        self.lidar_pub = self.create_publisher(PointCloud2, 'lidar', 10)
-        self.beacon_pub = self.create_publisher(PointCloud2, 'beacon', 10)
+        self.lidar_pub = self.create_publisher(PointCloud2, "lidar", 10)
+        self.beacon_pub = self.create_publisher(PointCloud2, "beacon", 10)
 
-        self.pos_viz_pub = self.create_publisher(Marker, 'visualization_marker', 10)
-        self.beacon_viz_pub = self.create_publisher(PointCloud2, 'beacon_viz', 10)
-        self.lidar_viz_pub = self.create_publisher(PointCloud2, 'lidar_viz', 10)
+        self.pos_viz_pub = self.create_publisher(Marker, "visualization_marker", 10)
+        self.beacon_viz_pub = self.create_publisher(PointCloud2, "beacon_viz", 10)
+        self.lidar_viz_pub = self.create_publisher(PointCloud2, "lidar_viz", 10)
 
         self.lidar_pub_timer = self.create_timer(0.1, self.lidar_publish_cb)
         self.beacon_pub_timer = self.create_timer(0.1, self.beacon_publish_cb)
@@ -47,70 +49,80 @@ class PhysicsSimNode(Node):
         self.vel_true = np.array([0, 0, 0], dtype=float)
         self.pos_est = np.array([0, 0, 0], dtype=float)
         self.accel = np.array([0, 0, 0], dtype=float)
-    
+
     def control_signal_cb(self, msg: Vector3):
         self.accel = np.array([msg.x, msg.y, msg.z])
 
     def _apply_2d_noise(self, points: np.array, std_dev: float):
         noisy_points = []
         for point in points:
-            noisy_point = np.array([
-                point[0] + np.random.normal(0, std_dev),
-                point[1] + np.random.normal(0, std_dev),
-                0,
-            ])
+            noisy_point = np.array(
+                [
+                    point[0] + np.random.normal(0, std_dev),
+                    point[1] + np.random.normal(0, std_dev),
+                    0,
+                ]
+            )
             noisy_points.append(noisy_point)
         return noisy_points
-    
 
     def lidar_publish_cb(self):
-        lidar_points = MAP.calc_lidar_point_cloud(self.pos_true,
-                                                    self.lidar_delta_theta,
-                                                    self.lidar_r_max,
-                                                    self.lidar_r_min)
+        lidar_points = MAP.calc_lidar_point_cloud(
+            self.pos_true, self.lidar_delta_theta, self.lidar_r_max, self.lidar_r_min
+        )
         lidar_points = self._apply_2d_noise(lidar_points, self.lidar_std_dev)
         header = Header()
         header.stamp = self.get_clock().now().to_msg()
-        header.frame_id = 'map'
+        header.frame_id = "map"
         lidar_msg = point_cloud2.create_cloud_xyz32(header, lidar_points)
         self.lidar_pub.publish(lidar_msg)
 
         lidar_points_world = []
         for point in lidar_points:
-            lidar_points_world.append(np.array([
-                point[0] + self.pos_true[0],
-                point[1] + self.pos_true[1],
-                0,
-            ]))
-        lidar_points_world_msg = point_cloud2.create_cloud_xyz32(header, lidar_points_world)
+            lidar_points_world.append(
+                np.array(
+                    [
+                        point[0] + self.pos_true[0],
+                        point[1] + self.pos_true[1],
+                        0,
+                    ]
+                )
+            )
+        lidar_points_world_msg = point_cloud2.create_cloud_xyz32(
+            header, lidar_points_world
+        )
         self.lidar_viz_pub.publish(lidar_points_world_msg)
-
 
     def beacon_publish_cb(self):
         beacon_positions = MAP.calc_beacon_positions(self.pos_true)
         beacon_positions = self._apply_2d_noise(beacon_positions, self.beacon_std_dev)
         header = Header()
         header.stamp = self.get_clock().now().to_msg()
-        header.frame_id = 'map'
+        header.frame_id = "map"
         beacon_msg = point_cloud2.create_cloud_xyz32(header, beacon_positions)
         self.beacon_pub.publish(beacon_msg)
 
         beacon_positions_world = []
         for point in beacon_positions:
-            beacon_positions_world.append(np.array([
-                point[0] + self.pos_true[0],
-                point[1] + self.pos_true[1],
-                0,
-            ]))
-        beacon_positions_world_msg = point_cloud2.create_cloud_xyz32(header, beacon_positions_world)
+            beacon_positions_world.append(
+                np.array(
+                    [
+                        point[0] + self.pos_true[0],
+                        point[1] + self.pos_true[1],
+                        0,
+                    ]
+                )
+            )
+        beacon_positions_world_msg = point_cloud2.create_cloud_xyz32(
+            header, beacon_positions_world
+        )
         self.beacon_viz_pub.publish(beacon_positions_world_msg)
 
-    
     def publish_true_pos(self):
         marker = Marker()
         marker.header.stamp = self.get_clock().now().to_msg()
-        marker.header.frame_id = 'map'
-        marker.ns = 'true_position'
+        marker.header.frame_id = "map"
+        marker.ns = "true_position"
         marker.id = 0
         marker.type = Marker.SPHERE
         marker.action = Marker.ADD
@@ -119,7 +131,7 @@ class PhysicsSimNode(Node):
         marker.pose.position.x = self.pos_true[0]
         marker.pose.position.y = self.pos_true[1]
         marker.pose.position.z = self.pos_true[2]
-        
+
         # No rotation needed
         marker.pose.orientation.x = 0.0
         marker.pose.orientation.y = 0.0
@@ -138,7 +150,6 @@ class PhysicsSimNode(Node):
         marker.color.a = 1.0  # Fully opaque
 
         self.pos_viz_pub.publish(marker)
-    
 
     def sim_update_cb(self):
         self.pos_true += self.vel_true * self.sim_dt
@@ -153,10 +164,11 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        print('Exiting...')
+        print("Exiting...")
     finally:
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
